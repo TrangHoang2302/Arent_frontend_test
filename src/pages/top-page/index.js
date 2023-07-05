7d3c3e43c172778df2ee2e465dc27e7d6ec0a47f
TrangHoang2302 <tranghoang1001@gmail.com>
2023-07-05 21:53:01 +0700

    [TASK-1] Add Top Page

--- src/pages/top-page/index.js
+++ src/pages/top-page/index.js
@@ -4,104 +4,102 @@
  * url: src/pages/top-page/index.js
  *************************/
 
-import React, { useMemo } from "react";
+import React from "react";
 import { Grid, Box, Typography } from "@mui/material";
 import Hex from "../../component/hex";
+import BodyFatGraph from "./body-fat-graph";
 import moment from "moment";
 
+/**
+ * data button to transit to input
+ * @type {[{icon: string, label: string, id: string},{icon: string, label: string, id: string},{icon: string, label: string, id: string},{icon: string, label: string, id: string}]}
+ */
+const arrButton = [
+    {
+        icon: "",
+        label: "Morning",
+        id: "btnMorning",
+    },
+    {
+        icon: "",
+        label: "Lunch",
+        id: "btnLunch",
+    },
+    {
+        icon: "",
+        label: "Dinner",
+        id: "btnDinner",
+    },
+    {
+        icon: "",
+        label: "Snack",
+        id: "btnSnack",
+    },
+];
+
+/**
+ * data list meal history
+ * @type {[{src: *, time: moment.Moment, id: string},{src: *, time: moment.Moment, id: string},{src: *, time: moment.Moment, id: string},{src: *, time: moment.Moment, id: string}]}
+ */
+const arrMealHistory = [
+    {
+        time: moment().format(),
+        src: require("../../assets/image/m01.jpg"),
+        id: "m01",
+    },
+    {
+        time: moment().format(),
+        src: require("../../assets/image/m02.jpg"),
+        id: "m02",
+    },
+    {
+        time: moment().format(),
+        src: require("../../assets/image/m03.jpg"),
+        id: "m03",
+    },
+    {
+        time: moment().format(),
+        src: require("../../assets/image/l03.jpg"),
+        id: "l03",
+    },
+];
+
+const styles = {
+    positionAbsolute: {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        border: "2px solid white",
+        borderRadius: 100,
+        width: 180,
+        height: 180,
+        color: "white",
+    },
+    box1: {
+        height: 312,
+        objectFit: "cover",
+    },
+    box2: {
+        width: "100%",
+        height: 312,
+        objectFit: "cover",
+    },
+    box3: {
+        width: 234,
+        height: 241,
+        objectFit: "cover",
+    },
+};
+
 const TopPage = () => {
-    /**
-     * data button to transit to input
-     * @type {[{icon: string, label: string, id: string},{icon: string, label: string, id: string},{icon: string, label: string, id: string},{icon: string, label: string, id: string}]}
-     */
-    const arrButton = [
-        {
-            icon: "",
-            label: "Morning",
-            id: "btnMorning",
-        },
-        {
-            icon: "",
-            label: "Lunch",
-            id: "btnLunch",
-        },
-        {
-            icon: "",
-            label: "Dinner",
-            id: "btnDinner",
-        },
-        {
-            icon: "",
-            label: "Snack",
-            id: "btnSnack",
-        },
-    ];
-
-    /**
-     * function random date
-     * @type {moment.Moment}
-     */
-    const renderDate = useMemo(() => {
-        return moment().add(Math.random() * 10, "days");
-    }, []);
-
-    /**
-     * data list meal history
-     * @type {[{src: *, time: moment.Moment, id: string},{src: *, time: moment.Moment, id: string},{src: *, time: moment.Moment, id: string},{src: *, time: moment.Moment, id: string}]}
-     */
-    const arrMealHistory = [
-        {
-            time: renderDate,
-            src: require("../../assets/image/m01.jpg"),
-            id: "m01",
-        },
-        {
-            time: renderDate,
-            src: require("../../assets/image/m02.jpg"),
-            id: "m02",
-        },
-        {
-            time: renderDate,
-            src: require("../../assets/image/m03.jpg"),
-            id: "m03",
-        },
-        {
-            time: renderDate,
-            src: require("../../assets/image/l03.jpg"),
-            id: "l03",
-        },
-    ];
-
     return (
         <Grid container justifyContent="center">
             {/*Date-Achievement rate*/}
             <Box sx={{ position: "relative" }}>
                 <Grid item xs={4}>
-                    <Box
-                        component="img"
-                        sx={{
-                            height: 312,
-                            objectFit: "cover",
-                        }}
-                        alt=""
-                        src={require("../../assets/image/d01.jpg")}
-                    />
-                    <Box
-                        sx={{
-                            position: "absolute",
-                            top: "50%",
-                            left: "50%",
-                            transform: "translate(-50%, -50%)",
-                            border: "2px solid white",
-                            borderRadius: 100,
-                            width: 180,
-                            height: 180,
-                            color: "white",
-                        }}
-                        display="flex"
-                        justifyContent="center"
-                        alignItems="center"
-                    >
+                    <Box component="img" sx={styles.box1} alt="" src={require("../../assets/image/d01.jpg")} />
+                    <Box sx={styles.positionAbsolute} display="flex" justifyContent="center" alignItems="center">
                         <Typography>{`${moment().format("DD-MM")} - 75%}`}</Typography>
                     </Box>
                 </Grid>
@@ -109,16 +107,7 @@ const TopPage = () => {
 
             {/*Body weight - Body fat percentage graph*/}
             <Grid item xs={8}>
-                <Box
-                    component="img"
-                    sx={{
-                        width: "100%",
-                        height: 312,
-                        objectFit: "cover",
-                    }}
-                    alt=""
-                    src={require("../../assets/image/chart.jpg")}
-                />
+                <BodyFatGraph />
             </Grid>
 
             {/*Button to transit to input*/}
@@ -134,17 +123,7 @@ const TopPage = () => {
             <Grid item xs={10}>
                 <Box display="flex" justifyContent="center" gap={2} alignItems="center">
                     {arrMealHistory.map((history, index) => (
-                        <Box
-                            key={index}
-                            component="img"
-                            sx={{
-                                width: 234,
-                                height: 241,
-                                objectFit: "cover",
-                            }}
-                            alt=""
-                            src={history.src}
-                        />
+                        <Box key={index} component="img" sx={styles.box3} alt="" src={history.src} />
                     ))}
                 </Box>
             </Grid>
