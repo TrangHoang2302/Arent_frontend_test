b81010ecf9afa1deb0bc915e043a8540d467f4aa
Trang Hoàng <tranghoang1001@gmail.com>
2023-07-06 15:13:03 +0700

    Updates

---
+++ precache-manifest.ce81caaf6f8ac95f0a4b1b76014c28a6.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374408814efbb90fd560a057df312cf1",
+    "url": "/Arent_frontend_test/index.html"
+  },
+  {
+    "revision": "0c89c2698d43731b141b",
+    "url": "/Arent_frontend_test/static/js/2.1f1e6886.chunk.js"
+  },
+  {
+    "revision": "4dc01d0c3c3d84dd9a62ee457d70908f",
+    "url": "/Arent_frontend_test/static/js/2.1f1e6886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b395337ee61d63804499",
+    "url": "/Arent_frontend_test/static/js/main.4a04dc88.chunk.js"
+  },
+  {
+    "revision": "9191414ac6f0e9d99c26",
+    "url": "/Arent_frontend_test/static/js/runtime-main.ec6a4290.js"
+  },
+  {
+    "revision": "7cdd1ea9472cbf9aa075537b464b361d",
+    "url": "/Arent_frontend_test/static/media/MyRecommend-1.7cdd1ea9.jpg"
+  },
+  {
+    "revision": "13ca9aa383805226686d577198d83ddb",
+    "url": "/Arent_frontend_test/static/media/MyRecommend-2.13ca9aa3.jpg"
+  },
+  {
+    "revision": "9967a9c2b3e4b4a5793bf2f98726ca47",
+    "url": "/Arent_frontend_test/static/media/MyRecommend-3.9967a9c2.jpg"
+  },
+  {
+    "revision": "87c4f3cefa8ebbcbed1a941a86e96243",
+    "url": "/Arent_frontend_test/static/media/column-1.87c4f3ce.jpg"
+  },
+  {
+    "revision": "c0debe256748af91387b13aa955137ff",
+    "url": "/Arent_frontend_test/static/media/column-2.c0debe25.jpg"
+  },
+  {
+    "revision": "a6e86e741b4d46c709645814d1ad4275",
+    "url": "/Arent_frontend_test/static/media/column-3.a6e86e74.jpg"
+  },
+  {
+    "revision": "644de179033a157742a1ebc5153be33b",
+    "url": "/Arent_frontend_test/static/media/column-4.644de179.jpg"
+  },
+  {
+    "revision": "e920e87b15fd89ad4d0603fecf75608d",
+    "url": "/Arent_frontend_test/static/media/column-5.e920e87b.jpg"
+  },
+  {
+    "revision": "615e5008e89d03a1276dfb9d33d4dbb2",
+    "url": "/Arent_frontend_test/static/media/column-6.615e5008.jpg"
+  },
+  {
+    "revision": "4a3ab47e70569405691119d898c8e275",
+    "url": "/Arent_frontend_test/static/media/column-7.4a3ab47e.jpg"
+  },
+  {
+    "revision": "0d4159f0d204e274ce1774bbb52e694e",
+    "url": "/Arent_frontend_test/static/media/column-8.0d4159f0.jpg"
+  },
+  {
+    "revision": "8be1141d32d8f0ab8ee7ba60345d391e",
+    "url": "/Arent_frontend_test/static/media/d01.8be1141d.jpg"
+  },
+  {
+    "revision": "0df812d7e8ca2f56a171e28439f8e6b5",
+    "url": "/Arent_frontend_test/static/media/l03.0df812d7.jpg"
+  },
+  {
+    "revision": "a816ae17398d4466b8293ef376853c60",
+    "url": "/Arent_frontend_test/static/media/m01.a816ae17.jpg"
+  },
+  {
+    "revision": "55244fbb337d086fcdd864838a6ddfe5",
+    "url": "/Arent_frontend_test/static/media/m02.55244fbb.jpg"
+  },
+  {
+    "revision": "0094a4216de0a171b0b6a4d52b0e96a5",
+    "url": "/Arent_frontend_test/static/media/m03.0094a421.jpg"
+  }
+]);
